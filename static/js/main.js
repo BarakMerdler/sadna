9702fce42cd623dc3c6534e2920d1fe76c2c7299
Einar <einarulzary@gmail.com>
2020-04-25 17:24:40 +0300

    Home page changed

--- static/js/main.js
+++ static/js/main.js
@@ -120,29 +120,8 @@
 
 })(jQuery);
 
-  //main table
-$(function() {
-	$(".column").sortable({
-		connectWith: ".column",
-		handle: ".portlet-header",
-		cancel: ".portlet-toggle",
-		placeholder: "portlet-placeholder ui-corner-all"
-	});
 
-	$(".portlet")
-		.addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-		.find(".portlet-header")
-		.addClass("ui-widget-header ui-corner-all");
 
-	$(".portlet-toggle").click(function() {
-		var icon = $(this);
-		icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
-		icon
-			.closest(".portlet")
-			.find(".portlet-content")
-			.toggle();
-	});
-});
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -188,3 +167,4 @@ function viewSearch(){
     x.style.display = "block";
   }
 }
+
