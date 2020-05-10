09fc78f350f107d2c6b9b204175b937b9c4305d5
Einar <einarulzary@gmail.com>
2020-05-10 12:13:17 +0300

    Fix home page

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def home():
             petTemp.append(setAnimal)
         if pet['place'] == 'waiting':
             petWaiting.append(setAnimal)
-    return render_template('/api.html', petCage=petCage, petOper=petOper, petTemp=petTemp, petWaiting=petWaiting)
+    return render_template('/home.html', petCage=petCage, petOper=petOper, petTemp=petTemp, petWaiting=petWaiting)
 
 # Route to add pet (new or no)
 @app.route('/addpet', methods=['GET', 'POST'])
